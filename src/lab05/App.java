caa5c82fa2d83b6a0de6173193eaf7d3143f4be3
Prachna <ponpolprachna12@gmail.com>
2025-01-14 22:45:20 +0700

    V1.4: Stable

--- src/lab05/App.java
+++ src/lab05/App.java
@@ -9,9 +9,21 @@ package lab05;
 public class App {
     public static void main(String[] args) {
         Subject se = new Subject();
+        // Quick Check
+        System.out.println("\rSubject : " + se.getSubjectName());
+        System.out.println("Code : " + se.getSubjectCode());
         System.out.println("Initial Credit: " + se.getNumberOfCredits());
+        System.out.println("Season: " + se.getSeason() + "\n");
+        // Modified
         se.setNumberOfCredits(90);
+        se.setSubjectCode(001);
+        se.setSubjectName("Malware");
+        // Re-check
+        System.out.println("Subject : " + se.getSubjectName());
+        System.out.println("Code : " + se.getSubjectCode());
         System.out.println("After credit: " + se.getNumberOfCredits());
         System.out.println("Season: " + se.getSeason());
+        // null test
+        se.setSubjectName("");
     }
 }
