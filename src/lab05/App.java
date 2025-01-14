6bf2aa7a9a30012e596501af2611aa35f96795b7
Prachna <ponpolprachna12@gmail.com>
2025-01-14 23:43:26 +0700

    Adding testing functionality

--- src/lab05/App.java
+++ src/lab05/App.java
@@ -3,12 +3,13 @@ package lab05;
 /**
  * Describe about Current-Subjects in Class.
  * @author Prachna
- * @version 1.2
+ * @version 1.5
  * @modified 14-01-2025
  */
 public class App {
     public static void main(String[] args) {
         Subject se = new Subject();
+        Session schedule = new Session();
         // Quick Check
         System.out.println("\rSubject : " + se.getSubjectName());
         System.out.println("Code : " + se.getSubjectCode());
@@ -25,5 +26,12 @@ public class App {
         System.out.println("Season: " + se.getSeason());
         // null test
         se.setSubjectName("");
+        // Session implementation
+        System.out.println("Class: " + schedule.getSessionTitle());
+        System.out.println("Session start: " + schedule.getSessionStart());
+        System.out.println("Session end: " + schedule.getSessionEnd());
+        // modified
+        schedule.setDuration(1, 2);
+        schedule.setSessionTitle("Godly Class");
     }
 }
