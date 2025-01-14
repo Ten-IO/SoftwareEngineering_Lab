d4addb41a0ad3365826c3bc3f71f36b9b28b993b
Ten_IO <poktepouten.info@protonmail.com>
2025-01-14 23:51:35 +0700

    handle exception try-catch

--- src/lab05/App.java
+++ src/lab05/App.java
@@ -1,15 +1,16 @@
 package lab05;
 
 /**
- * Describe about Current-Subjects in Class.
- * @author Prachna
- * @version 1.5
+ * Describe about Current-Subject-Session in Class.
+ * @author Ten
+ * @version 1.6
  * @modified 14-01-2025
  */
 public class App {
     public static void main(String[] args) {
         Subject se = new Subject();
         Session schedule = new Session();
+        try{
         // Quick Check
         System.out.println("\rSubject : " + se.getSubjectName());
         System.out.println("Code : " + se.getSubjectCode());
@@ -26,12 +27,19 @@ public class App {
         System.out.println("Season: " + se.getSeason());
         // null test
         se.setSubjectName("");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Input error: " + e.getMessage());
+        }
         // Session implementation
         System.out.println("Class: " + schedule.getSessionTitle());
         System.out.println("Session start: " + schedule.getSessionStart());
         System.out.println("Session end: " + schedule.getSessionEnd());
         // modified
+        try{
         schedule.setDuration(1, 2);
         schedule.setSessionTitle("Godly Class");
+        } catch (IllegalArgumentException e) {
+            System.out.println("Logic error: " + e.getMessage());
+        }
     }
 }
